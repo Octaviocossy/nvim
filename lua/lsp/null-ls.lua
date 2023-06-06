4cfb8fde2c5966ff0fdd33f562ff3d1e191ecf6b
Octaviocossy <octaviocossytorquati@gmail.com>
2023-06-06 18:59:37 -0300

    📦 rewrites keymap formatter null-ls

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,15 +10,14 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.prettier,
-    formatting.eslint_d,
-    formatting.stylua,
-    diagnostics.eslint_d,
-  },
+	debug = false,
+	sources = {
+		formatting.prettier,
+		formatting.eslint_d,
+		formatting.stylua,
+		diagnostics.eslint_d,
+	},
 })
 
 -- Keymaps
-
-vim.keymap.set("n", "<Leader>f", ":lua vim.lsp.buf.format({ timeout_ms = 5000 })<Return>")
+vim.keymap.set("n", "<Leader>f", "<cmd>lua vim.lsp.buf.format({ timeout_ms = 5000 })<cr><esc>")
