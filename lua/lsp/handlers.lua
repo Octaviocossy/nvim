2a88ea064589d8b2f2490b043754952b5fb425ac
Octaviocossy <octaviocossytorquati@gmail.com>
2023-08-01 22:47:02 -0300

    🟢 LSP Saga changes

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -62,8 +62,17 @@ if not status then
 end
 
 saga.setup({
-	server_filetype_map = {
-		typescript = "typescript",
+	ui = {
+		border = "rounded",
+	},
+	symbol_in_winbar = {
+		enable = false,
+	},
+	lightbulb = {
+		enable = false,
+	},
+	outline = {
+		layout = "float",
 	},
 })
 
@@ -72,8 +81,8 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "H", "<Cmd>Lspsaga hover_doc<CR>", opts)
 vim.keymap.set("n", "gd", "<Cmd>Lspsaga goto_definition<CR>", opts)
 vim.keymap.set("n", "gt", "<Cmd>Lspsaga goto_type_definition<CR>", opts)
-vim.keymap.set("n", "sl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
-vim.keymap.set("n", "sf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+vim.keymap.set("n", ";d", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
+vim.keymap.set("n", ";f", "<Cmd>Lspsaga lsp_finder<CR>", opts)
 vim.keymap.set("n", "sr", "<Cmd>Lspsaga rename<CR>", opts)
 vim.keymap.set("n", "d[", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 vim.keymap.set("n", "d]", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
