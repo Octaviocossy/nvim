3de4720a3024bf465576afd0d2c0cc4a4c966f51
Octaviocossy <octaviocossytorquati@gmail.com>
2023-08-06 00:26:47 -0300

    🚫 Removes unnecessary code

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -39,22 +39,6 @@ M.setup = function()
 	})
 end
 
-local function lsp_highlight_document(client)
-	-- Set autocommands conditional on server_capabilities
-	if client.server_capabilities.documentHighlight then
-		vim.api.nvim_exec(
-			[[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-			false
-		)
-	end
-end
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
