0a9a68c8c07367b3928c024b1d2b71188b08bd04
Octaviocossy <octaviocossytorquati@gmail.com>
2023-08-22 10:46:57 -0300

    🟠 update colorscheme

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -16,7 +16,7 @@ return require("packer").startup(function()
 	use({ "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" })
 
 	-- Colorscheme
-	use({ "catppuccin/nvim", as = "catppuccin" })
+	use({ "ellisonleao/gruvbox.nvim" })
 
 	-- File explorer
 	use({
