4a9e9761904ac561dfa10dea2651ed8a9518779a
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2023-10-07 14:25:01 -0300

    ✨ Update lsp signs symbols

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -10,7 +10,7 @@ M.setup = function()
 	})
 
 	-- Diagnostic symbols in the sign column (gutter)
-	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 	for type, icon in pairs(signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
