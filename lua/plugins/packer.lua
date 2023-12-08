0af8383d01206abbfacf3126dea0df13b05ba721
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2023-12-07 21:40:40 -0300

    update colors

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -3,97 +3,97 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap =
-      fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-  vim.cmd([[packadd packer.nvim]])
+	packer_bootstrap =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd([[packadd packer.nvim]])
 end
 
 return require("packer").startup(function()
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Common utilities
-  use({ "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" })
+	-- Common utilities
+	use({ "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" })
 
-  -- Colorscheme
-  use { "catppuccin/nvim", as = "catppuccin" }
+	-- Colorscheme
+	use({ "craftzdog/solarized-osaka.nvim" })
 
-  -- File explorer
-  use({
-    "nvim-tree/nvim-tree.lua",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-  })
+	-- File explorer
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	})
 
-  -- Tmux nav
-  use("alexghergh/nvim-tmux-navigation")
+	-- Tmux nav
+	use("alexghergh/nvim-tmux-navigation")
 
-  -- Statusline
-  use("nvim-lualine/lualine.nvim")
+	-- Statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- Fuzzy file finder
-  use("nvim-telescope/telescope.nvim")
+	-- Fuzzy file finder
+	use("nvim-telescope/telescope.nvim")
 
-  -- Completion Plugins
-  use({
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp",
-    "saadparwaiz1/cmp_luasnip",
-  })
+	-- Completion Plugins
+	use({
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-nvim-lsp",
+		"saadparwaiz1/cmp_luasnip",
+	})
 
-  -- Snippets
-  use({
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-  })
+	-- Snippets
+	use({
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+	})
 
-  -- LSP
-  use({
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "jose-elias-alvarez/null-ls.nvim",
-    "onsails/lspkind-nvim",
-    "nvimdev/lspsaga.nvim",
-    "github/copilot.vim",
-  })
+	-- LSP
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"jose-elias-alvarez/null-ls.nvim",
+		"onsails/lspkind-nvim",
+		"nvimdev/lspsaga.nvim",
+		"github/copilot.vim",
+	})
 
-  -- Treesitter
-  use("nvim-treesitter/nvim-treesitter")
+	-- Treesitter
+	use("nvim-treesitter/nvim-treesitter")
 
-  -- Close tags and more
-  use({
-    "windwp/nvim-autopairs",
-    "windwp/nvim-ts-autotag",
-  })
+	-- Close tags and more
+	use({
+		"windwp/nvim-autopairs",
+		"windwp/nvim-ts-autotag",
+	})
 
-  -- Comments
-  use("numToStr/Comment.nvim")
+	-- Comments
+	use("numToStr/Comment.nvim")
 
-  -- Git
-  use("lewis6991/gitsigns.nvim")
+	-- Git
+	use("lewis6991/gitsigns.nvim")
 
-  -- Colorizer
-  use("norcalli/nvim-colorizer.lua")
+	-- Colorizer
+	use("norcalli/nvim-colorizer.lua")
 
-  -- Bufferline
-  use({ "akinsho/bufferline.nvim", tag = "*" })
+	-- Bufferline
+	use({ "akinsho/bufferline.nvim", tag = "*" })
 
-  -- better ui
-  use("folke/noice.nvim")
+	-- better ui
+	use("folke/noice.nvim")
 
-  -- better notifications
-  use("rcarriga/nvim-notify")
+	-- better notifications
+	use("rcarriga/nvim-notify")
 
-  -- better indent lines
-  use("lukas-reineke/indent-blankline.nvim")
+	-- better indent lines
+	use("lukas-reineke/indent-blankline.nvim")
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
