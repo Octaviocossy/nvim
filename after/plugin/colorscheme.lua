6ac1c0874f4603b2e39e327398ce54444320f20c
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-01-13 18:53:41 -0300

    🏗️ Update color theme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,44 +1,66 @@
-local colorscheme = "gruvbox"
+local colorscheme = "monokai-pro"
 
 local theme_status, theme = pcall(require, colorscheme)
 
 if not theme_status then
-	vim.notify("Colorscheme not found!")
-	vim.cmd([[
+  vim.notify("Colorscheme not found!")
+  vim.cmd([[
     colorscheme default
 		set background=dark
   ]])
-	return
+  return
 end
 
 theme.setup({
-	terminal_colors = true, -- add neovim terminal colors
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = false,
-		emphasis = false,
-		comments = false,
-		operators = false,
-		folds = false,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "hard", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {},
-	dim_inactive = false,
-	transparent_mode = true,
+  transparent_background = true,
+  terminal_colors = true,
+  devicons = true, -- highlight the icons of `nvim-web-devicons`
+  styles = {
+    comment = { italic = true },
+    keyword = { italic = true },       -- any other keyword
+    type = { italic = true },          -- (preferred) int, long, char, etc
+    storageclass = { italic = true },  -- static, register, volatile, etc
+    structure = { italic = true },     -- struct, union, enum, etc
+    parameter = { italic = true },     -- parameter pass in function
+    annotation = { italic = true },
+    tag_attribute = { italic = true }, -- attribute of tag in reactjs
+  },
+  filter = "pro",                      -- classic | octagon | pro | machine | ristretto | spectrum
+  -- Enable this will disable filter option
+  day_night = {
+    enable = false,            -- turn off by default
+    day_filter = "pro",        -- classic | octagon | pro | machine | ristretto | spectrum
+    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+  },
+  inc_search = "background",   -- underline | background
+  background_clear = {
+    -- "float_win",
+    "toggleterm",
+    "telescope",
+    -- "which-key",
+    "renamer",
+    "notify",
+    "nvim-tree",
+    -- "neo-tree",
+    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+  }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+  plugins = {
+    bufferline = {
+      underline_selected = false,
+      underline_visible = false,
+    },
+    indent_blankline = {
+      context_highlight = "default", -- default | pro
+      context_start_underline = false,
+    },
+  },
+  ---@param c Colorscheme
+  override = function(c) end,
 })
 
 local present, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not present then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
 end
