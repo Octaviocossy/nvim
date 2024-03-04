0756ea0866d7fc0ac02f082f4584b47d23f38782
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-03-04 14:47:21 -0300

    update lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -5,24 +5,51 @@ if not status_ok then
 end
 
 lualine.setup({
-
 	options = {
-		theme = "catppuccin",
-		globalstatus = true,
+    globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
-		section_separators = { left = "█", right = "█" },
+		disabled_filetypes = {},
 	},
 	sections = {
-		lualine_b = {
-			{ "branch", icon = "" },
-			"diff",
-			"diagnostics",
-		},
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
 		lualine_c = {
-			{ "filename", path = 1 },
+			{
+				"filename",
+				file_status = true, -- displays file status (readonly status, modified status)
+				path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
 		},
 		lualine_x = {
+			{
+				"diff",
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			},
+			"encoding",
 			"filetype",
 		},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- displays file status (readonly status, modified status)
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = { "fugitive" },
 })
