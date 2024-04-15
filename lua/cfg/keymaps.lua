5654656375b30455a92306d826d0958ab15aee56
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-04-14 22:23:11 -0300

    add dressing, lazygiy and ufo plugins

--- lua/cfg/keymaps.lua
+++ lua/cfg/keymaps.lua
@@ -94,4 +94,60 @@ nnoremap("<leader>f", function()
   require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format the current buffer" })
 
+-- Diagnostics
+
+-- Goto next diagnostic of any severity
+nnoremap("]d", function()
+  vim.diagnostic.goto_next({})
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous diagnostic of any severity
+nnoremap("[d", function()
+  vim.diagnostic.goto_prev({})
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next error diagnostic
+nnoremap("]e", function()
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous error diagnostic
+nnoremap("[e", function()
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next warning diagnostic
+nnoremap("]w", function()
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous warning diagnostic
+nnoremap("[w", function()
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+  vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Open the diagnostic under the cursor in a float window
+nnoremap("<leader>d", function()
+  vim.diagnostic.open_float({
+    border = "rounded",
+  })
+end)
+
+-- nvim-ufo keybinds
+nnoremap("zR", function()
+  require("ufo").openAllFolds()
+end)
+nnoremap("zM", function()
+  require("ufo").closeAllFolds()
+end)
+
+-- Git keymaps --
+nnoremap("<leader>lg", ":LazyGit<cr>")
+
 return M
