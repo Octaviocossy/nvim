f394d7525faf9d237066b0bf77cd6f699a668785
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-04-21 23:37:39 -0300

    mod: comment gh copilot plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    event = { "BufEnter" },
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = false,
-        },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    enabled = true,
-    event = { "BufEnter" },
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
+  --   {
+  --     "zbirenbaum/copilot.lua",
+  --     enabled = true,
+  --     event = { "BufEnter" },
+  --     config = function()
+  --       require("copilot").setup({
+  --         suggestion = {
+  --           enabled = false,
+  --         },
+  --         panel = { enabled = false },
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "zbirenbaum/copilot-cmp",
+  --     enabled = true,
+  --     event = { "BufEnter" },
+  --     dependencies = { "zbirenbaum/copilot.lua" },
+  --     config = function()
+  --       require("copilot_cmp").setup()
+  --     end,
+  --   },
 }
