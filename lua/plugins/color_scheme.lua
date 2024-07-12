b3fa14fe551534c3a6e4bf9a046fe27e0e6dee63
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-07-11 23:28:33 -0300

    add tokyo theme

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 }
