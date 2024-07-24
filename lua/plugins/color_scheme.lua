98e359ffe70147b90d5433e35df2e0a4f2b22b2d
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-07-23 21:59:20 -0300

    add catppuccin theme

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,17 +1,48 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-				styles = {
-					sidebars = "transparent",
-					floats = "transparent",
-				},
-			})
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          harpoon = true,
+          indent_blankline = {
+            enabled = false,
+            scope_color = "sapphire",
+            colored_indent_levels = false,
+          },
+          mason = true,
+          native_lsp = { enabled = true },
+          noice = true,
+          notify = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin-mocha")
+
+      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+      for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+        vim.api.nvim_set_hl(0, group, {})
+      end
+    end,
+  },
 }
