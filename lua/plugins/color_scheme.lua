dfc8349e69851b35c1d9a44b23a8c7d67dbb8ba0
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-09-28 14:12:58 -0300

    update theme

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,22 +1,45 @@
 return {
+  -- {
+  --   "datsfilipe/vesper.nvim",
+  --   config = function()
+  --     require("vesper").setup({
+  --       transparent = true, -- Boolean: Sets the background to transparent
+  --       italics = {
+  --         comments = false, -- Boolean: Italicizes comments
+  --         keywords = false, -- Boolean: Italicizes keywords
+  --         functions = false, -- Boolean: Italicizes functions
+  --         strings = false, -- Boolean: Italicizes strings
+  --         variables = false, -- Boolean: Italicizes variables
+  --       },
+  --       overrides = {},  -- A dictionary of group names, can be a function returning a dictionary or a table.
+  --       palette_overrides = {},
+  --     })
+  --
+  --     vim.cmd("colorscheme vesper")
+  --
+  --   end,
+  -- },
   {
-    "datsfilipe/vesper.nvim",
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("vesper").setup({
-        transparent = true, -- Boolean: Sets the background to transparent
-        italics = {
-          comments = false, -- Boolean: Italicizes comments
-          keywords = false, -- Boolean: Italicizes keywords
-          functions = false, -- Boolean: Italicizes functions
-          strings = false, -- Boolean: Italicizes strings
-          variables = false, -- Boolean: Italicizes variables
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "transparent", -- style for sidebars, see below
+          floats = "transparent", -- style for floating windows
         },
-        overrides = {},  -- A dictionary of group names, can be a function returning a dictionary or a table.
-        palette_overrides = {},
       })
 
-      vim.cmd("colorscheme vesper")
-
+      vim.cmd("colorscheme tokyonight-night")
     end,
   },
 }
