847f13bcfbd5ade70b271d83681eb2a810821dce
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-10-02 19:05:54 -0300

    add gruvbox theme

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,22 +1,53 @@
 return {
+  -- {
+  --   "datsfilipe/vesper.nvim",
+  --   config = function()
+  --     require("vesper").setup({
+  --       transparent = true, -- Boolean: Sets the background to transparent
+  --       italics = {
+  --         comments = false, -- Boolean: Italicizes comments
+  --         keywords = false, -- Boolean: Italicizes keywords
+  --         functions = false, -- Boolean: Italicizes functions
+  --         strings = false, -- Boolean: Italicizes strings
+  --         variables = false, -- Boolean: Italicizes variables
+  --       },
+  --       overrides = {},  -- A dictionary of group names, can be a function returning a dictionary or a table.
+  --       palette_overrides = {},
+  --     })
+  --
+  --     vim.cmd("colorscheme vesper")
+  --
+  --   end,
+  -- },
   {
-    "datsfilipe/vesper.nvim",
+    "ellisonleao/gruvbox.nvim",
     config = function()
-      require("vesper").setup({
-        transparent = true, -- Boolean: Sets the background to transparent
-        italics = {
-          comments = false, -- Boolean: Italicizes comments
-          keywords = false, -- Boolean: Italicizes keywords
-          functions = false, -- Boolean: Italicizes functions
-          strings = false, -- Boolean: Italicizes strings
-          variables = false, -- Boolean: Italicizes variables
+      -- Default options:
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
         },
-        overrides = {},  -- A dictionary of group names, can be a function returning a dictionary or a table.
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
         palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
       })
-
-      vim.cmd("colorscheme vesper")
-
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 }
