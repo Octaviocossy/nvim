d1569f8958b6e3406cc82736adc9cca31be9e8f3
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2025-01-18 23:44:59 -0300

    update theme

--- lua/plugins/color_scheme.lua
+++ lua/plugins/color_scheme.lua
@@ -1,104 +1,14 @@
 return {
   {
-    "datsfilipe/vesper.nvim",
+    "projekt0n/github-nvim-theme",
     config = function()
-      require("vesper").setup({
-        transparent = true, -- Boolean: Sets the background to transparent
-        italics = {
-          comments = false, -- Boolean: Italicizes comments
-          keywords = false, -- Boolean: Italicizes keywords
-          functions = false, -- Boolean: Italicizes functions
-          strings = false, -- Boolean: Italicizes strings
-          variables = false, -- Boolean: Italicizes variables
+      require("github-theme").setup({
+        options = {
+          transparent = true,
         },
-        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-        palette_overrides = {},
       })
 
-      -- setup must be called before loading
-      vim.cmd.colorscheme("vesper")
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true,
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true,
-        contrast = "hard",
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = true,
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "auto", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-          light = "latte",
-          dark = "mocha",
-        },
-        transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false,          -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15,        -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false,          -- Force no italic
-        no_bold = false,            -- Force no bold
-        no_underline = false,       -- Force no underline
-        styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" },  -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-          -- miscs = {}, -- Uncomment to turn off hard-coded styles
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      })
+      vim.cmd("colorscheme github_dark")
     end,
   },
 }
